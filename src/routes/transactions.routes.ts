f6ad8768c9891a00161be8f3769435933cb13538
mguidoti <marcus.guidoti@gmail.com>
2020-11-17 19:58:44 -0300

    feat: Code for passing the delete test

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import { getCustomRepository } from 'typeorm';
+import AppError from '../errors/AppError';
 
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import CreateTransactionService from '../services/CreateTransactionService';
-// import DeleteTransactionService from '../services/DeleteTransactionService';
+import DeleteTransactionService from '../services/DeleteTransactionService';
 // import ImportTransactionsService from '../services/ImportTransactionsService';
 
 const transactionsRouter = Router();
@@ -44,7 +45,13 @@ transactionsRouter.post('/', async (request, response) => {
 });
 
 transactionsRouter.delete('/:id', async (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const deleteTransactionService = new DeleteTransactionService();
+
+  await deleteTransactionService.execute(id);
+
+  return response.status(200).json({ message: 'Successfully deleted!' });
 });
 
 transactionsRouter.post('/import', async (request, response) => {
--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,26 @@
-// import AppError from '../errors/AppError';
+import { isUuid } from 'uuidv4';
+import { getRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+import Transaction from '../models/Transaction';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    // to do
+    const transactionsRepository = getRepository(Transaction);
+
+    if (!isUuid(id)) {
+      throw new AppError('Provided id is not a valid uuid', 400);
+    }
+
+    const transaction = await transactionsRepository.find({
+      where: { id },
+    });
+
+    if (transaction.length === 0) {
+      throw new AppError("Provided id doesn't exist", 400);
+    }
+
+    await transactionsRepository.remove(transaction);
   }
 }
 
